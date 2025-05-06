c41e0deda2b611ed9ec16ecca4f86c400a0193bc
marchel0603 <marchelinoboyoh@gmail.com>
2025-05-07 03:45:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,4 +54,6 @@ def handle_message(data):
     emit('message', {'msg': msg}, room=room)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    import eventlet
+    import eventlet.wsgi
+    socketio.run(app, host='0.0.0.0', port=5000)
